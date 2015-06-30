587f1c291ea963eed471dcc04cae218621e9d4f8
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-06-30 02:17:26 +0200

    Controller implemented

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@
  */
 
 // Token Bucket Params
-var TokenRate = 1;
-var PacketRate = 6;
+var TokenRate = 4;
+var PacketRate = 3;
+var MaxPacketRate = 7; // Visual artifacts there after
+var MaxTokenRate = 7;  // Visual artifacts there after
 var MaxTokens = 20;     // # of token objects defined in HTML
 var MaxPackets = 20;    // # of Packet objects defined in HTML
 var TimeScale = 5;     // All rates are TimeScale times of real time
@@ -18,7 +20,7 @@ var aTokenPacketBottom = "-65px";   // Final value of bottom for animator token
 // Animation times
 var bucketToPacket = 0.2;    // Time taken for token to enter packet from bucket
 var sourceToBucket = 0.5;    // Time taken for packet to reach bucket from source
-var bucketToNetwork = 0.8;   // Time taken for packet to reach network from bucket
+var bucketToNetwork = 0.5;   // Time taken for packet to reach network from bucket
 var bucketToTrash = 0.5;   // Time taken for packet to reach trash from bucket
 
 // Objects
@@ -28,6 +30,99 @@ var mPacketsTransit = [];
 var mAnimToken;
 var mAvailTokens = 0;
 
+/**
+ * Initializes the application. Called in window.onload()
+ */
+function init(){
+
+    // Init Packet objects
+    for(var i=1; i<=MaxPackets; i++){
+        mPackets.push(new Packet(document.getElementById("packet-" + i), new TimelineLite(), false));
+    }
+
+    // Init token objects and their visibility
+    for(var j=1; j<=MaxTokens; j++){
+        mTokens.push(new Token(document.getElementById("token-" + j)));
+        mTokens[j-1].consume();
+    }
+
+    // Init animator token
+    mAnimToken = new TokenAnimator(document.getElementById("token-animate"), new TimelineLite());
+    mAnimToken.reset();
+
+    // Update rate UI
+    document.getElementById("token-rate").innerText = TokenRate;
+    document.getElementById("packet-rate").innerText = PacketRate;
+
+    // Init Token and Packet generator
+    TokenGenerator();
+    PacketGenerator();
+}
+
+/**
+ * Controller functions
+ */
+function increasePacketRate(){
+    if(PacketRate < MaxPacketRate){
+        PacketRate++;
+        document.getElementById("packet-rate").innerText = PacketRate;
+    }
+}
+function decreasePacketRate(){
+    if(PacketRate > 1){
+        PacketRate--;
+        document.getElementById("packet-rate").innerText = PacketRate;
+    }
+}
+function increaseTokenRate(){
+    if(TokenRate < MaxTokenRate){
+        TokenRate++;
+        document.getElementById("token-rate").innerText = TokenRate;
+    }
+}
+function decreaseTokenRate(){
+    if(TokenRate > 1){
+        TokenRate--;
+        document.getElementById("token-rate").innerText = TokenRate;
+    }
+}
+
+/**
+ * Generates a new token if possible
+ * @constructor
+ */
+function TokenGenerator() {
+    if(mAvailTokens < MaxTokens) {
+        generateToken();
+    }
+    setTimeout(function(){TokenGenerator()}, TimeScale * 1000 / TokenRate);
+}
+
+/**
+ * Generates a new packet to network traffic. If possible.
+ * @constructor
+ */
+function PacketGenerator(){
+    if(mPackets.length > 0){
+        mPacketsTransit.push(mPackets.pop());
+        mPacketsTransit[mPacketsTransit.length - 1].animateToBucket();
+    }
+    setTimeout(function(){PacketGenerator()}, TimeScale * 1000 / PacketRate);
+}
+
+function generateToken(){
+    mTokens[mAvailTokens].generate();
+    mAvailTokens++;
+}
+
+function consumeToken(){
+    mAvailTokens--;
+    mTokens[mAvailTokens].consume();
+}
+
+/** ----------------------------------------------------------------
+ ------------------------  Object representations ------------------
+ -------------------------------------------------------------------**/
 /**
  * Denotes a Packet
  * @param domobj
@@ -167,55 +262,5 @@ function TokenAnimator(domobj, tl){
         this.consume();
         tl.to(domobj, 0, {bottom: aTokenDefaultBottom});
     }
-}
-
-/**
- * Initializes the application. Called in window.onload()
- */
-function init(){
-
-    // Init Packet objects
-    for(var i=1; i<=MaxPackets; i++){
-        mPackets.push(new Packet(document.getElementById("packet-" + i), new TimelineLite(), false));
-    }
-
-    // Init token objects and their visibility
-    for(var j=1; j<=MaxTokens; j++){
-        mTokens.push(new Token(document.getElementById("token-" + j)));
-        mTokens[j-1].consume();
-    }
-
-    // Init animator token
-    mAnimToken = new TokenAnimator(document.getElementById("token-animate"), new TimelineLite());
-    mAnimToken.reset();
-
-    // Init Token and Packet generator
-    TokenGenerator();
-    PacketGenerator();
-}
-
-function TokenGenerator() {
-    if(mAvailTokens < MaxTokens) {
-        generateToken();
-    }
-    setTimeout(function(){TokenGenerator()}, TimeScale * 1000 / TokenRate);
-}
-
-function PacketGenerator(){
-    if(mTokens.length > 0){
-        mPacketsTransit.push(mPackets.pop());
-        mPacketsTransit[mPacketsTransit.length - 1].animateToBucket();
-    }
-    setTimeout(function(){PacketGenerator()}, TimeScale * 1000 / PacketRate);
-}
-
-function generateToken(){
-    mTokens[mAvailTokens].generate();
-    mAvailTokens++;
-}
-
-function consumeToken(){
-    mAvailTokens--;
-    mTokens[mAvailTokens].consume();
 }
 
